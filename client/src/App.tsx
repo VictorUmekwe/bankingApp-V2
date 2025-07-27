6639d47356a2ec10cb1238f6b45735eb440e999a
Victor Umekwe <flexzy4u@gmail.com>
2025-07-27 15:48:51 +0100

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center">
+        <Button>click me</Button>
+    </div>
+  )
+}
+
+export default App
